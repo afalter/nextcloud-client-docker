ebb72040da6a7e340e2c25015b67abef09dde53e
afalter <48165619+afalter@users.noreply.github.com>
2020-01-07 19:19:41 +0100

    Changed to use USER for executing command

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/ash
 
 # ensure, that the sync dir exists and is owned by the user
 [ -d /media/nextcloud ] || mkdir -p /media/nextcloud
-echo "chown -R 99:100 /media/nextcloud"
-chown -R 99:100 /media/nextcloud
+echo "chown -R $USER:$GROUP /media/nextcloud"
+chown -R $USER:$GROUP /media/nextcloud
 
 while true
 do
-	nextcloudcmd $( [ "$NC_HIDDEN" == true ] && echo "-h" ) $( [ "$NC_SILENT" == true ] && echo "--silent" ) $( [ "$NC_TRUST_CERT" == true ] && echo "--trust" ) --non-interactive -u $NC_USER -p $NC_PASS /media/nextcloud $NC_URL
+	/bin/su -s /bin/ash $USER -c 'nextcloudcmd $( [ "$NC_HIDDEN" == true ] && echo "-h" ) $( [ "$NC_SILENT" == true ] && echo "--silent" ) $( [ "$NC_TRUST_CERT" == true ] && echo "--trust" ) --non-interactive -u $NC_USER -p $NC_PASS /media/nextcloud $NC_URL'
 	
 	#check if exit!
 	if [ "$NC_EXIT" = true ] ; then
