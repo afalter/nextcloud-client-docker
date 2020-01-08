cdeb8dff034ce01d0d37dda9e78051e8d6ed1188
afalter <48165619+afalter@users.noreply.github.com>
2020-01-08 23:40:49 +0100

    Use umask necessary for unraid shares

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ chown -R $USER:$GROUP /media/nextcloud
 
 while true
 do
-	/bin/su -s /bin/ash $USER -c 'nextcloudcmd $( [ "$NC_HIDDEN" == true ] && echo "-h" ) $( [ "$NC_SILENT" == true ] && echo "--silent" ) $( [ "$NC_TRUST_CERT" == true ] && echo "--trust" ) --non-interactive -u $NC_USER -p $NC_PASS /media/nextcloud $NC_URL'
+	/bin/su -s /bin/ash $USER -c 'umask 0000 && nextcloudcmd $( [ "$NC_HIDDEN" == true ] && echo "-h" ) $( [ "$NC_SILENT" == true ] && echo "--silent" ) $( [ "$NC_TRUST_CERT" == true ] && echo "--trust" ) --non-interactive -u $NC_USER -p $NC_PASS /media/nextcloud $NC_URL'
 	
 	#check if exit!
 	if [ "$NC_EXIT" = true ] ; then
