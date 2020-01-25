75cbe2653e8212fb9d3740dfaf7721484815d6c7
afalter <48165619+afalter@users.noreply.github.com>
2020-01-25 22:12:23 +0100

    Removed user stuff

--- run.sh
+++ run.sh
@@ -1,14 +1,8 @@
 #!/bin/ash
 
-# ensure, that the sync dir exists and is owned by the user
-[ -d /media/nextcloud ] || mkdir -p /media/nextcloud
-echo "chown -R $USER:$GROUP /media/nextcloud"
-chown -R $USER:$GROUP /media/nextcloud
-chmod -R 777 /media/nextcloud
-
 while true
 do
-	/bin/su -s /bin/ash $USER -c 'umask 0000 && nextcloudcmd $( [ "$NC_HIDDEN" == true ] && echo "-h" ) $( [ "$NC_SILENT" == true ] && echo "--silent" ) $( [ "$NC_TRUST_CERT" == true ] && echo "--trust" ) --non-interactive -u $NC_USER -p $NC_PASS /media/nextcloud $NC_URL'
+	nextcloudcmd $( [ "$NC_HIDDEN" == true ] && echo "-h" ) $( [ "$NC_SILENT" == true ] && echo "--silent" ) $( [ "$NC_TRUST_CERT" == true ] && echo "--trust" ) --non-interactive -u $NC_USER -p $NC_PASS /media/nextcloud $NC_URL
 	
 	#check if exit!
 	if [ "$NC_EXIT" = true ] ; then
